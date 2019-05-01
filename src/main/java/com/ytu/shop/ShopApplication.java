fbb94db045bbf405477a632ad136786634eba0de
nic <nicahead@gmail.com>
2019-05-01 16:11:21 +0800

    ssl

--- src/main/java/com/ytu/shop/ShopApplication.java
+++ src/main/java/com/ytu/shop/ShopApplication.java
@@ -5,7 +5,6 @@ import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
@@ -17,12 +16,12 @@ public class ShopApplication {
 
     // 在某配置类中添加如下内容
     // 监听的http请求的端口,需要在application配置中添加http.port=端口号  如80
-    @Value("${http.port}")
-    Integer httpPort;
+//    @Value("${http.port}")
+    Integer httpPort = 80;
 
     //正常启用的https端口 如443
-    @Value("${server.port}")
-    Integer httpsPort;
+//    @Value("${server.port}")
+    Integer httpsPort = 443;
 
     // springboot2 写法
     @Bean
